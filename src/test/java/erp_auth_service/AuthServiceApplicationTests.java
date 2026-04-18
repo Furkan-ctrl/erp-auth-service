e5d9012f72f05fc234d137160bfe437aafa0f5bd
Furkan-ctrl <furkannagiyev20071@gmail.com>
2026-04-18 21:16:20 +0400

    Initial project setup including .gitignore

---
+++ src/test/java/erp_auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package erp_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
