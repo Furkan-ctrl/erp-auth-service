dec43a4f3fbb36d0166486601fbe6a28023a6f8f
Furkan-ctrl <furkannagiyev20071@gmail.com>
2026-04-23 07:54:55 +0400

    Created the model package with User and Role classes

---
+++ src/main/java/com/erp/auth/model/Role.java
@@ -0,0 +1,5 @@
+package com.erp.auth.model;
+
+public enum Role {
+    ADMIN , USER
+}
---
+++ src/main/java/com/erp/auth/model/User.java
@@ -0,0 +1,59 @@
+package com.erp.auth.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.management.relation.Role;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+
+    public class User {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(nullable = false, unique = true, length = 100)
+        private String email;
+
+        @Column(nullable = false)
+        private String passwordHash;
+
+        @Column(nullable = false, length = 50)
+        private String fullName;
+
+        @ElementCollection(fetch = FetchType.EAGER)
+        @Enumerated(EnumType.STRING)
+        @CollectionTable(
+                name = "user_roles",
+                joinColumns = @JoinColumn(name = "user_id")
+        )
+        @Column(name = "role")
+        private Set<Role> roles;
+
+        @Column(nullable = false, updatable = false)
+        private LocalDateTime createdAt;
+
+        @Column(nullable = false)
+        private LocalDateTime updatedAt;
+
+        @PrePersist
+        protected void onCreate() {
+            this.createdAt = LocalDateTime.now();
+            this.updatedAt = LocalDateTime.now();
+        }
+
+        @PreUpdate
+        protected void onUpdate() {
+            this.updatedAt = LocalDateTime.now();
+        }
+}
