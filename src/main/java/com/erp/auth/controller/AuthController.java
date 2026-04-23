cf57873a94df1e4a357c2cdb67d31a1ee97527de
Furkan-ctrl <furkannagiyev20071@gmail.com>
2026-04-23 18:01:07 +0400

    Created the AuthController

---
+++ src/main/java/com/erp/auth/controller/AuthController.java
@@ -0,0 +1,57 @@
+package com.erp.auth.controller;
+
+import com.erp.auth.dto.AuthResponse;
+import com.erp.auth.dto.LoginRequest;
+import com.erp.auth.dto.RefreshRequest;
+import com.erp.auth.dto.RegisterRequest;
+import com.erp.auth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Slf4j
+public class AuthController {
+    private final AuthService authService;
+
+ @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid RegisterRequest request){
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+ @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid LoginRequest request){
+       AuthResponse response = authService.login(request);
+       return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponse> refresh(
+            @Valid RefreshRequest request) {
+        AuthResponse response = authService.refresh(request);
+        return ResponseEntity.ok(response);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
